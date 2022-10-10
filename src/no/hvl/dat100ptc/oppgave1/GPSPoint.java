e552db84cfeb32e90f3c64ce2f2188a6a732b280
august <august@10.0.0.121>
2022-10-10 14:39:22 +0200

    tester

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -17,57 +17,53 @@ public class GPSPoint {
 		this.latitude = latitude;
 		this.latitude = longitude;
 		this.elevation = elevation;
-		// Fjern linjene med throw new etterhvert som metodene implementeres
-		// throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
 
 	}
 
 	// TODO - get/set metoder
 	public int getTime() {
-		
-		//throw new UnsupportedOperationException(TODO.method());
+
 		return time;
 	}
 
 	public void setTime(int time) {
+		
 		this.time = time;		
-		//throw new UnsupportedOperationException(TODO.method());
-
 	}
 
 	public double getLatitude() {
+		
 		return latitude;
-		//throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setLatitude(double latitude) {
+		
 		this.latitude = latitude;
-		//throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public double getLongitude() {
+		
 		return longitude;
-		// throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setLongitude(double longitude) {
+		
 		this.longitude = longitude;
-		// throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public double getElevation() {
+		
 		return elevation;
-		// throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setElevation(double elevation) {
+		
 		this.elevation = elevation;
-		// throw new UnsupportedOperationException(TODO.method());
 		
 	}
 	
@@ -77,7 +73,7 @@ public class GPSPoint {
 		
 		// TODO - start
 		return Integer.toString(time) + " (" + Double.toString(latitude) + "," + Double.toString(longitude)
-		+ ") " + Double.toString(elevation);
+		+ ") " + Double.toString(elevation) + "\n";
 		
 		
 		
